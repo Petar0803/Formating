950c43c41d8def70817baee61c8fd9f311c8f6d3
Students <Students@DESKTOP-JO0JC7P>
2025-07-01 10:19:27 +0200

    Add project files.

---
+++ Formating/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Formating
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            double[] doubles = { 1.237, 2.4876, 3.5, 4.01234 }; // Плаваща запетая, дробно число
+
+            List<String> formatted = doubles.Select(
+              d => string.Format("{0:F3}", d)
+            ).ToList();
+
+            formatted.ForEach(Console.WriteLine);
+
+        }
+    }
+}
